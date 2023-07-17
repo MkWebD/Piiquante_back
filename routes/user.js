d3ccc76c34a242589f1a0b4f614abe7773bcf842
Kevin Mas <kmas@thess-corp.fr>
2023-07-17 08:07:14 +0200

    fix: removing sensitive data

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,21 @@
 // External requires
 const express = require("express");
-const bouncer = require("express-bouncer")(30000, 120000, 3)
+const bouncer = require("express-bouncer")(30000, 10000, 10);
 
 // Importing middleware
 const passwordValidator = require("../middleware/validators/passwordValidator");
 const emailValidator = require("../middleware/validators/emailValidator");
 
 // Importing methods for users
-const {signUp} = require("../controllers/user/signup");
-const {logIn} = require("../controllers/user/login");
+const { signUp } = require("../controllers/user/signup");
+const { logIn } = require("../controllers/user/login");
 
 // Creating express Router
 const router = express.Router();
 
 // Custom error message for bouncer
 bouncer.blocked = function (req, res, next, remaining) {
-  res.status(429).json({message: "Too many requests have been made. Please wait " + remaining/1000 + " seconds."});
+	res.status(429).json({ message: "Too many requests have been made. Please wait " + remaining / 1000 + " seconds." });
 };
 
 // Routing for users
